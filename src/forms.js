6801aa7597fb8855122247278bbd35916cbdbc4e
mantel5 <a28310@svalero.com>
2025-12-01 18:53:27 +0100

    examen

---
+++ src/forms.js
@@ -0,0 +1,139 @@
+
+
+const handleFillCountry = _.debounce((ev) => {
+    // only show matched events
+
+    const node = ev.target.parentNode.getElementsByClassName('search-box')[0]
+    node.style.display = 'initial'
+    node.innerHTML = ''
+
+    let inputText = ev.target.value.toLowerCase()
+    console.log(`search for ${inputText}`);
+
+    for (let country of countryList) {
+        let row = document.createElement('div')
+        row.innerText = country
+        row.onclick = selectCountry
+
+        node.appendChild(row)
+    }
+  }, 300);
+
+//
+function validateName(event) {
+    const name = event.target.value
+    console.log('validate name: ' + name);
+    
+    if (name.length <= 8 ){
+        showElementWithClassName(event.target, 'invalid-feedback')
+        hideElementWithClassName(event.target, 'valid-feedback')
+    }else if (name.length > 8){
+        showElementWithClassName(event.target, 'valid-feedback')
+        hideElementWithClassName(event.target, 'invalid-feedback')
+    }else if (name.length = null) {
+        showElementWithClassName(event.target, 'invalid-feedback')
+    } 
+    return false
+}
+
+function validatePassword(event) {
+    const password = event.target.value
+    const Numeros = /[0-9]/.test(password)
+    const Minusculas = /[a-z]/.test(password)
+    const Mayusculas = /[A-Z]/.test(password)
+    console.log('validate password: ' + password);
+    const passwordCorrecto =  Numeros && password.length > 8 &&Minusculas && Mayusculas
+
+    if(passwordCorrecto){
+        showElementWithClassName(event.target, 'valid-feedback')
+        hideElementWithClassName(event.target, 'invalid-feedback')
+    }else {
+        showElementWithClassName(event.target, 'invalid-feedback')
+        hideElementWithClassName(event.target, 'valid-feedback')
+    }
+
+
+    return passwordCorrecto
+}
+
+function validateEmail(event) {
+    const email = event.target.value
+    const regex = /^[^\s@] + @[^\s@]+\.[^\s@]+$/;
+    const emailCorrecto = regex.test(email);
+    console.log('validate email: ' + email);
+    
+    if (emailCorrecto){
+        showElementWithClassName(event.target, 'invalid-feedback')
+        hideElementWithClassName(event.target, 'valid-feedback')
+
+    }else if (emailCorrecto >3){
+        showElementWithClassName(event.target, 'invalid-feedback')
+    }else{
+        showElementWithClassName(event.target, 'valid-feedback')
+        hideElementWithClassName(event.target, 'invalid-feedback')
+    }
+    return false
+    
+    
+
+}
+
+
+// general register
+function register(event) {
+    const email = event.target.value
+    const regex = /^[^\s@] + @[^\s@]+\.[^\s@]+$/;
+    const password = event.target.value
+    const name = event.target.value
+
+    if (email.length > 1, email.length = regex, password.length > 8, name.length > 8){
+        fetch('http://localhost:3000/', {
+            method: 'POST',
+            body: JSON.stringify({
+                'name': 'sample'
+                
+            }),
+            headers: {
+                'Content-type': 'application/json'
+            },
+        })
+        event.preventDefault();
+        return false;
+
+    }
+   
+    
+}
+
+// utility functions
+//nombre
+function showElementWithClassName(node, className) {
+    node.parentNode.getElementsByClassName(className)[0].style.display = 'initial'
+}
+function hideElementWithClassName(node, className) {
+    node.parentNode.getElementsByClassName(className)[0].style.display = 'none'
+}
+
+function selectCountry(event) {
+    console.log(event);
+    document.forms[0].country.value = event.target.innerText
+
+    const node = document.getElementsByClassName('search-box')[0]
+    node.style.display = 'none'
+    node.innerHTML = ''
+}
+
+function init() {
+    let items = document.getElementsByClassName('valid-feedback')
+    for (const item of items) {
+        item.style.display = 'none'
+    }
+    items = document.getElementsByClassName('invalid-feedback')
+    for (const item of items) {
+        item.style.display = 'none'
+    }
+
+    document.getElementsByClassName('search-box')[0].style.display = 'none'
+}
+
+init()
